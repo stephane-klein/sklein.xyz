e9040e166b1757951c61307b5a6442ac4201b9a4
Stéphane Klein <contact@stephane-klein.info>
2022-08-19 23:32:37 +0200

    Add header anchor #13

--- src/routes/garden/[page_slug]/+page.server.js
+++ src/routes/garden/[page_slug]/+page.server.js
@@ -1,13 +1,25 @@
 import * as path from 'path';
 import * as cheerio from 'cheerio';
 import MarkdownIt from 'markdown-it';
+import markdownItAnchor from 'markdown-it-anchor';
 import matter_read from '$lib/utils';
 
 const markdownIt = new MarkdownIt({
     html: true,
     linkify: true,
     typographer: true
-});
+})
+    .use(
+        markdownItAnchor,
+        {
+            permalink: markdownItAnchor.permalink.linkInsideHeader({
+                symbol: `
+                    <span aria-hidden="true">¶</span>
+                `,
+                placement: 'after'
+            })
+        }
+    );
 
 export async function load({ params }) {
     const filename = path.join(
