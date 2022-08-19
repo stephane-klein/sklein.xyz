b9e565fcda01a0272aaa4d55359548b4d001ae7c
Stéphane Klein <contact@stephane-klein.info>
2022-08-19 23:13:45 +0200

    Display created_at in Garden pages #11

--- src/routes/garden/[page_slug]/+page.svelte
+++ src/routes/garden/[page_slug]/+page.svelte
@@ -2,7 +2,11 @@
     export let data;
 </script>
 
-
 <h1>{data.frontmatter?.title ?? 'No title'}</h1>
+
+<dl>
+    <dt>Publié le :&nbsp;</dt>
+    <dd>{ data.frontmatter?.created_at ?? '-'}</dd>
+</dl>
 
 {@html data.body}
