76f78d090e9cd6ba3707d30504d9eb9ae8dfcbc5
Stéphane Klein <contact@stephane-klein.info>
2023-03-09 06:39:45 +0100

    Upgrade glob from 8.0.3 and 9.2.1

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,11 +1,11 @@
-import { default as glob } from "glob";
+import { globSync } from "glob";
 import matter_read from "$lib/utils";
 
 export async function load() {
     const re = new RegExp(/^contents\/(?<path>.*)\.md$/);
     return {
         posts: await Promise.all(
-            Object.values(glob.sync("contents/blog/*.md")).map(async (filename) => {
+            Object.values(globSync("contents/blog/*.md")).map(async (filename) => {
                 return {
                     path: `${filename.match(re).groups.path}/`,
                     frontmatter: matter_read(filename).data,
@@ -13,7 +13,7 @@ export async function load() {
             })
         ),
         pages: await Promise.all(
-            Object.values(glob.sync("contents/garden/*.md")).map(async (filename) => {
+            Object.values(globSync("contents/garden/*.md")).map(async (filename) => {
                 return {
                     path: `/${filename.match(re).groups.path}/`,
                     frontmatter: matter_read(filename).data,
