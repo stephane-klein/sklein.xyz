e95438ae01e07daea6e3f232ae7b7440aa60b136
Stéphane Klein <contact@stephane-klein.info>
2023-04-03 00:05:03 +0200

    Add avatar

--- src/routes/fr/layout.svelte
+++ src/routes/fr/layout.svelte
@@ -1,26 +1,30 @@
 <!-- See https://github.com/sveltejs/kit/issues/627 to understand why +layout.svelte isn't used -->
 <script>
     import "../../screen.css";
+    import Avatar from "$lib/assets/avatar.png?w=50&webp";
 </script>
 
 <article>
-    <nav>
-        <a href="/fr/">Accueil</a>
-        <span>|</span>
-        <a href="/en/">English home page</a>
-        <span>|</span>
-        <a href="https://github.com/stephane-klein?tab=repositories">Projets</a>
-        <!-- Todo, see https://github.com/stephane-klein/sklein.xyz/issues/58
-        <span>|</span>
-        <a href="/">Now</a>
-        -->
-        <span>|</span>
-        <a href="https://cv.stephane-klein.info">CV</a>
-        <!-- Todo, see https://github.com/stephane-klein/sklein.xyz/issues/59
-        <span>|</span>
-        <a href="/">RSS</a>
-        -->
-    </nav>
+    <header>
+        <a href="/"><img src={Avatar} /></a>
+        <nav>
+            <a href="/fr/">Accueil</a>
+            <span>|</span>
+            <a href="/en/">English home page</a>
+            <span>|</span>
+            <a href="https://github.com/stephane-klein?tab=repositories">Projets</a>
+            <!-- Todo, see https://github.com/stephane-klein/sklein.xyz/issues/58
+            <span>|</span>
+            <a href="/">Now</a>
+            -->
+            <span>|</span>
+            <a href="https://cv.stephane-klein.info">CV</a>
+            <!-- Todo, see https://github.com/stephane-klein/sklein.xyz/issues/59
+            <span>|</span>
+            <a href="/">RSS</a>
+            -->
+        </nav>
+    </header>
     <main>
         <slot />
     </main>
