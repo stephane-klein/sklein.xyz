e9a0ee05e0cc105af9b767cba0437148a82c6b70
stephane-klein <contact@stephane-klein.info>
2024-05-16 16:40:29 +0200

    Fix bad server name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")/../"
 
 docker push stephaneklein/sklein.xyz:latest
 
-ssh root@perier.servers.stephane-klein.info 'bash -s' << 'EOF'
+ssh root@triton.servers.stephane-klein.info 'bash -s' << 'EOF'
     cd /srv/sklein_xyz/;
     docker-compose pull sklein_xyz
     docker-compose up -d sklein_xyz
