9414c7eea65a9a70a250394d57116f483c698691
stephane-klein <contact@stephane-klein.info>
2024-09-07 14:54:17 +0200

    Coding style

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-node";
+import preprocess from "svelte-preprocess";
 
-import { importAssets } from 'svelte-preprocess-import-assets'
+import { importAssets } from "svelte-preprocess-import-assets";
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    extensions: ['.svelte', '.md'],
+    extensions: [".svelte", ".md"],
     preprocess:[
         preprocess({
             postcss: true
