89db1f03c665975e51b12e3d8983ca7f7053a5a6
stephane-klein <contact@stephane-klein.info>
2024-09-07 15:00:48 +0200

    Fix coding style

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 const config = {
-	plugins: [
-		enhancedImages(),
+    plugins: [
+        enhancedImages(),
         sveltekit()
     ]
 };
